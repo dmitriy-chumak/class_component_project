18186e3018e4ef42474ca5caa539f2eb210bc795
Dmitriy Chumak <chumak.exceedteam@gmail.com>
2022-11-09 10:39:38 +0300

    new: initial commit

---
+++ src/components/CardOnList/CardOnList.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+class CardOnList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default CardOnList;
